068f2addc24620d8e890924585059c8b17c5e79f
Bill <bill.somen@randstadsolutions.io>
2025-07-14 08:41:01 -0400

    fix: correction on hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,8 +3,6 @@ import { motion } from 'framer-motion';
 // import bgImage from '/assets/img/photos/bg23.png'; // Adjust path based on your project structure
 import doodle1 from '/assets/img/svg/doodle1.svg';
 import doodle2 from '/assets/img/svg/doodle2.svg';
-import appStoreButton from '/assets/img/photos/button-appstore.svg';
-import googlePlayButton from '/assets/img/photos/button-google-play.svg';
 // import devicesImage from '/assets/img/photos/devices3.png';
 // import devicesImage2x from '/assets/img/photos/devices3@2x.png';
 import type { sectionProps } from '../types/common';
